e9aa32461ec86ae10e467cd0f229485431440467
K7413dS433d <khaledsaied2002@gmail.com>
2025-02-10 10:35:41 +0200

    fix parts and fix iusse of original part comes again

--- codex_stock/overrides/purchase_reciept_stock.py
+++ codex_stock/overrides/purchase_reciept_stock.py
@@ -4,27 +4,27 @@ import frappe
 def on_submit_purchase_receipt(doc, method):
     # Define parts and their quantities per item type
     item_parts = {
-        "ديك": {
-            "قنصة ديك": 1,
-            "كبدة ديك": 1,
-            "هيكل ديك": 1,
-            "صدور ديك": 2,
-            "وراك ديك": 2,
-            "دبوس صدر ديك": 2,
-            "جناح ديك": 2,
-            "رقبة ديك": 1,
+        "ديك حي": {
+            "قنصة": 1,
+            "كبدة": 1,
+            "هيكل": 1,
+            "صدور": 2,
+            "وراك": 2,
+            "دبوس صدر": 2,
+            "جناح": 2,
+            "رقبة بجلد و رأس": 1,
             "ريش ناعم": 1,
             "ريش سلاح": 1
         },
-        "هيكل ديك": {
+        "هيكل": {
             "قطع لحم": 1
         },
-        "وراك ديك": {
-            "شيش ديك": 1,
-            "زلموكه ديك": 1,
+        "وراك": {
+            "شيش": 1,
+            "زلموكه": 1,
             "دبوس ورك": 1
         },
-        "صدور ديك": {
+        "صدور": {
             "بانيه": 1
         }
     }
@@ -40,14 +40,15 @@ def on_submit_purchase_receipt(doc, method):
 
             # Add each part with the appropriate quantity multiplied by the received quantity
             for part, qty_per_unit in item_parts[item.item_code].items():
-                stock_entry.append(
-                    "items",
-                    {
-                        "item_code": part,
-                        "qty": qty_per_unit * item.qty,  # Calculate total quantity for the received item
-                        "t_warehouse": item.warehouse,
-                    },
-                )
+                if part != item.item_code:
+                    stock_entry.append(
+                        "items",
+                        {
+                            "item_code": part,
+                            "qty": qty_per_unit * item.qty,  # Calculate total quantity for the received item
+                            "t_warehouse": item.warehouse,
+                        },
+                    )
 
             # Submit the Stock Entry to update stock
             stock_entry.insert()
