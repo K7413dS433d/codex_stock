b9687b042673f0043f10d90e36b10d777877acbd
K7413dS433d <khaledsaied2002@gmail.com>
2025-02-10 11:30:53 +0200

    repack the original item from db to remove it

--- codex_stock/overrides/purchase_reciept_stock.py
+++ codex_stock/overrides/purchase_reciept_stock.py
@@ -38,17 +38,26 @@ def on_submit_purchase_receipt(doc, method):
             stock_entry.stock_entry_type = "Material Receipt"
             stock_entry.to_warehouse = item.warehouse  # Use the warehouse from the Purchase Receipt
 
+            # Remove the original item (Material Issue) repack concept
+            stock_entry.append(
+                "items",
+                {
+                    "item_code": item.item_code,  # Remove the same item received
+                    "qty": item.qty,  # Remove the same quantity received
+                    "s_warehouse": item.warehouse,  # Deduct from the same warehouse
+                },
+            )
+
             # Add each part with the appropriate quantity multiplied by the received quantity
             for part, qty_per_unit in item_parts[item.item_code].items():
-                if part != item.item_code:
-                    stock_entry.append(
-                        "items",
-                        {
-                            "item_code": part,
-                            "qty": qty_per_unit * item.qty,  # Calculate total quantity for the received item
-                            "t_warehouse": item.warehouse,
-                        },
-                    )
+                stock_entry.append(
+                    "items",
+                    {
+                        "item_code": part,
+                        "qty": qty_per_unit * item.qty,  # Calculate total quantity for the received item
+                        "t_warehouse": item.warehouse,
+                    },
+                )
 
             # Submit the Stock Entry to update stock
             stock_entry.insert()
